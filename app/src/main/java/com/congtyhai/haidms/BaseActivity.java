578bc031d83d237dc3b4676405335ca2a23975cc
namlhn <lenam.leo@gmail.com>
2017-08-21 16:46:54 +0700

    nam

--- app/src/main/java/com/congtyhai/haidms/BaseActivity.java
+++ app/src/main/java/com/congtyhai/haidms/BaseActivity.java
@@ -1,11 +1,20 @@
 package com.congtyhai.haidms;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
+import android.Manifest;
 import com.congtyhai.app.AppController;
 import com.congtyhai.di.component.ActivityComponent;
 import com.congtyhai.di.component.DaggerActivityComponent;
@@ -15,18 +24,15 @@ import com.congtyhai.util.AnimationHelper;
 import com.congtyhai.util.ApiInterface;
 import com.congtyhai.util.Commons;
 import com.congtyhai.util.SharedPrefsHelper;
-
+import com.sdsmdg.tastytoast.TastyToast;
 import javax.inject.Inject;
-import javax.inject.Singleton;
-
-import dagger.Provides;
 import retrofit2.Retrofit;
 
 /**
  * Created by HAI on 8/7/2017.
  */
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity implements LocationListener {
 
     @Inject
     protected Retrofit retrofit;
@@ -51,8 +57,12 @@ public class BaseActivity extends AppCompatActivity {
 
     protected Toolbar toolbar;
 
-    public BaseActivity() {
-    }
+    private LocationManager locationManager;
+
+    private String provider;
+
+    protected double lat = 0;
+    protected double lng = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +75,30 @@ public class BaseActivity extends AppCompatActivity {
         pDialog = new ProgressDialog(BaseActivity.this);
         pDialog.setTitle("Đang xử lý...");
         pDialog.setCancelable(false);
+
+        if (!checkGPS()) {
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        } else {
+            Location location = locationManager.getLastKnownLocation(provider);
+            if (location != null) {
+                lat = location.getLatitude();
+                lng = location.getLongitude();
+                onLocationChanged(location);
+            } else {
+                commons.makeToast(getApplicationContext(), "Location not found", TastyToast.INFO).show();
+            }
+        }
     }
 
     // dialog
@@ -103,4 +137,54 @@ public class BaseActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
     }
+
+    private boolean checkGPS() {
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        boolean enabled = service
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        return enabled;
+    }
+
+
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        } else {
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        lng = location.getLongitude();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        // Toast.makeText(this, "Enabled new provider " + provider, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        // Toast.makeText(this, "Disabled provider " + provider, Toast.LENGTH_SHORT).show();
+    }
+
 }
--- app/src/main/java/com/congtyhai/haidms/MainActivity.java
+++ app/src/main/java/com/congtyhai/haidms/MainActivity.java
@@ -7,20 +7,14 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.Gravity;
 import android.view.View;
 import android.view.MenuItem;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-
 import com.congtyhai.adapter.CheckinFunctionAdapter;
 import com.congtyhai.model.app.CheckInFunctionInfo;
 import com.congtyhai.view.NonScrollListView;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
